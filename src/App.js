11dfd6b3bbb2f42fb4e75b07e8913ea29748a8a5
Berkay Akbas <berkayakbas@gmail.com>
2022-08-02 08:32:19 +0300

    Change import and refacto App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.name = 'eslint';
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
