dd229e9f39c09001854cf1f8be1a020a60ff9444
Berkay Akbas <berkayakbas@gmail.com>
2022-08-10 12:36:26 +0300

    Add home and quote pages with router

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,31 +13,34 @@ const Calculator = () => {
   };
   const { total, next, operation } = calculator;
   return (
-    <div className="wrapper">
-      <div className="display">
-        {total}
-        {operation}
-        {next}
+    <div className="calc">
+      <p className="pcalc">Let&apos;s do some math!</p>
+      <div className="wrapper">
+        <div className="display">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <button type="button" className="ac" onClick={clickHandle} name="AC">AC</button>
+        <button type="button" className="plus-minus" onClick={clickHandle} name="+/-">+/-</button>
+        <button type="button" className="percent" onClick={clickHandle} name="%">%</button>
+        <button type="button" className="plus" onClick={clickHandle} name="÷">÷</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="7">7</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="8">8</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="9">9</button>
+        <button type="button" className="cross" onClick={clickHandle} name="x">x</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="4">4</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="5">5</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="6">6</button>
+        <button type="button" className="minus" onClick={clickHandle} name="-">-</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="1">1</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="2">2</button>
+        <button type="button" className="numbers" onClick={clickHandle} name="3">3</button>
+        <button type="button" className="plus" onClick={clickHandle} name="+">+</button>
+        <button type="button" className="zero" onClick={clickHandle} name="0">0</button>
+        <button type="button" className="period" onClick={clickHandle} name=".">.</button>
+        <button type="button" className="equal" onClick={clickHandle} name="=">=</button>
       </div>
-      <button type="button" className="ac" onClick={clickHandle} name="AC">AC</button>
-      <button type="button" className="plus-minus" onClick={clickHandle} name="+/-">+/-</button>
-      <button type="button" className="percent" onClick={clickHandle} name="%">%</button>
-      <button type="button" className="plus" onClick={clickHandle} name="÷">÷</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="7">7</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="8">8</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="9">9</button>
-      <button type="button" className="cross" onClick={clickHandle} name="x">x</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="4">4</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="5">5</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="6">6</button>
-      <button type="button" className="minus" onClick={clickHandle} name="-">-</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="1">1</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="2">2</button>
-      <button type="button" className="numbers" onClick={clickHandle} name="3">3</button>
-      <button type="button" className="plus" onClick={clickHandle} name="+">+</button>
-      <button type="button" className="zero" onClick={clickHandle} name="0">0</button>
-      <button type="button" className="period" onClick={clickHandle} name=".">.</button>
-      <button type="button" className="equal" onClick={clickHandle} name="=">=</button>
     </div>
   );
 };
