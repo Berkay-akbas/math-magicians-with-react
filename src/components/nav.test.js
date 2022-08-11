09757086d00b675677a1490ee14279a238012222
Berkay Akbas <berkayakbas@gmail.com>
2022-08-11 09:54:38 +0300

    Fix linter

--- src/components/nav.test.js
+++ src/components/nav.test.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Nav from './nav';
 
 test('renders the home page', () => {
-  render(<Router>
-    <Nav />
-  </Router>,);
+  render(
+    <Router>
+      <Nav />
+    </Router>,
+  );
   expect(screen.getByText('MATH MAGICIANS')).toBeInTheDocument();
 });
